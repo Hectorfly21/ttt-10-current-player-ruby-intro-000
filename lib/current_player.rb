6bb2d314cf165f4d4b27fc036469f1d73aac1336
Hector <hectorfly21@gmail.com>
2020-08-20 09:58:50 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
 counter = 0
 board.each do |player|
- if (player = "X") || (player = index "O")
+ if (player == "X") || (player == index "O")
    counter += 1
   puts "#{counter}"
   end
