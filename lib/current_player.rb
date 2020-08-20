49a8b1546e87a10af29254cd9509e7ab57e3ca2a
Hector <hectorfly21@gmail.com>
2020-08-20 10:31:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,9 +13,9 @@ current_player(board, turn_count)
 players = ["X", "O"]
 turn_count.each do |player|
  if (player % 2 == 0) || (player % 3 == 1)
-  puts "X"
+  return "X"
 else
-  puts "O"
+  return "O"
   end
 end
 current_player(board, turn_count)
